a8c65d84ed65518c9f6dffa64fca7a0b0a28a6ca
Faikor Kastrioti <fajkorkastrioti@gmail.com>
2022-05-03 06:43:57 +0200

    Modify script.js and style.css
    * script.js
    modified function createGridSquares() in order to create a 16x16 grid of
    squares.
    
    * style.css
    Made the appropriate changes to style.css. I.e., I commented out unnecessary
    code (like overflow: hidden, justify-content: space-between etc.).

--- script.js
+++ script.js
@@ -1,26 +1,18 @@
 /* Remove p tags from container, create grid squares and append it to container */
 function createGridSquares() {
   let container = document.querySelector(`.container`);
-  const containerStyles = getComputedStyle(container);
-  // let's take a look at **all** the styles of container!
-  console.log(containerStyles);
-  // that's a lot. How about just the ones related to us?
-  console.log(`width: ${containerStyles['width']}, height: ${containerStyles['height']}`);
-  console.log(typeof +containerStyles['width']);
+
   container.textContent = ``;
   let gridSquare = ``;
-  for(let i = 0; i < 64; i++) {
-    // for(let j = 0; j < 64; j++) {
+  for(let i = 0; i < 16; i++) {
+    for(let j = 0; j < 16; j++) {
       gridSquare = document.createElement(`div`);
       gridSquare.classList.add(`gridSquare`);
-      // gridSquare.setAttribute(`style`, `width: ${(+containerStyles['width'])/Math.sqrt(64)}px; height: ${(+containerStyles[`height`])/Math.sqrt(64)}px`);
+      /* I need to subtract 2px from the width and 2px from the height because of the border(1px on each side) */
+      gridSquare.setAttribute(`style`, `width:${450/16 - 2}px; height:${450/16- 2}px`);
 
-      // gridSquare.setAttribute(`style`, `width: ${(+containerStyles['width'])/Math.sqrt(64)}px; height: ${(+containerStyles[`height`])/Math.sqrt(64)}px`);
-      
-      console.log(containerStyles[`height`]);
       container.appendChild(gridSquare);
-
-    // }
+    }
   }
 }
 
