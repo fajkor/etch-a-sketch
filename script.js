452a53e130bb5686dac9582439fe941290c0745c
Faikor Kastrioti <fajkorkastrioti@gmail.com>
2022-05-05 20:22:23 +0200

    Modify style.css and script.js
    * style.css
    Add border to .gridSquare that doesn't affect its overall width. Width of
    .gridSquare remains the same, despite adding 1px border.
    
    * script.js
    Delete unnecessary code and comments.
    Set width of gridSquare as a percentage of the parent container, rather
    than in pixels.

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
 /* Remove p tags from container, create grid squares and append it to container */
 function createGridSquares() {
   let container = document.querySelector(`.container`);
-
   container.textContent = ``;
   let gridSquare = ``;
+
+  /* create 16x16 squares, give them size and append them to container */
   for(let i = 0; i < 16; i++) {
     for(let j = 0; j < 16; j++) {
       gridSquare = document.createElement(`div`);
       gridSquare.classList.add(`gridSquare`);
-      /* I need to subtract 2px from the width and 2px from the height because of the border(1px on each side) */
-      gridSquare.setAttribute(`style`, `width:${450/16 - 2}px; height:${450/16- 2}px`);
-
+      gridSquare.setAttribute(`style`, `width: ${100/16}%; height: ${100/16}%`);
       container.appendChild(gridSquare);
     }
   }
