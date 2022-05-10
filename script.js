dbd463cb35626f7ab082bd0f07970b67c1580ed2
Faikor Kastrioti <fajkorkastrioti@gmail.com>
2022-05-10 06:01:54 +0200

    Modify style.css and script.js
    * script.js
    Added function addClearGridButton().
    addClearGridButton() adds a new button and then puts an event listener
    to it. When clicked, asks the user to enter a grid size, stores the user
    input and then calls createGridSquares().
    
    * style.css
    Changes flex-direction to column in body.

--- script.js
+++ script.js
@@ -1,31 +1,55 @@
+const body = document.querySelector(`body`);
 const container = document.querySelector(`.container`);
-/* When the mouse pointer enters the container area, createGridSquares runs only once */
+let gridSize = 16;
+
+/* When the mouse pointer enters the container area, createGridSquares and addClearGridButton run only once */
 container.addEventListener(`mouseenter`, createGridSquares, {once: true});
-// console.log(gridSquare);
+container.addEventListener(`mouseenter`, addClearGridButton, {once: true});
+
 
 /* Clear container, create grid squares and append them to container */
 function createGridSquares() {
+
   container.textContent = ``;
   let gridSquare = ``;
   let gridSquareArray = [];
-
+  
   /* create 16x16 squares, push them to an array, give them size and append them to container */
-  for(let i = 0; i < 16; i++) {
-    for(let j = 0; j < 16; j++) {
+  for(let i = 0; i < gridSize; i++) {
+    for(let j = 0; j < gridSize; j++) {
       gridSquare = document.createElement(`div`);
+      /* I need to push the gridSquare nodes to an array, because later I am going to add an event listener to each one of them. */
       gridSquareArray.push(gridSquare);
       gridSquare.classList.add(`gridSquare`);
-      gridSquare.setAttribute(`style`, `width: ${100/16}%`);
+      gridSquare.setAttribute(`style`, `width: ${100/gridSize}%`);
       container.appendChild(gridSquare);
     }
   }
 
-  /* Add an event listener to each item of the array, and then execute the arrow function once that event is triggered. */
-  for (let i = 0; i < 256; i++) {
-    gridSquareArray[i].addEventListener(`mouseover`, ()=> {
+  /* Add an event listener to each item of the array, and then execute the arrow function when that event is triggered. */
+  for (let i = 0; i < (gridSize * gridSize); i++) {
+    gridSquareArray[i].addEventListener(`mouseover`, () => {
       gridSquareArray[i].style.backgroundColor = `black`;
     }, {once: true});
   }
+}
+
+/* Create Clear Grid button, append it, add event listener to it. */
+function addClearGridButton() {
+  let clearGrid = document.createElement(`button`);
+  clearGrid.classList.add(`clearGrid`);
+  clearGrid.setAttribute(`type`, `button`);
+  clearGrid.textContent = `Clear Grid`;
+  body.insertBefore(clearGrid, container);
+
+  clearGrid.addEventListener(`click`, () => {
+    gridSize = Number(prompt(`Please, enter grid size`, `1 - 100`));
+    if ((gridSize <= 0) || (gridSize > 100) || Number.isNaN(gridSize)) {
+      alert(`Invalid Input! Try again!`);
+    } else if (gridSize > 0 || gridSize <= 100) {
+      createGridSquares();
+    }
+  });
 }
 
 
